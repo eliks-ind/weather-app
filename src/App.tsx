fe10882188ef9e2a79f6958443dbc599298a55df
eliks-ind <20fantomas09@gmail.com>
2024-11-19 00:41:38 +0400

    Created simple logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import WeatherDisplay from "./components/WeatherDisplay";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const fetchWeather = async () => {
+    const API_KEY = "cb7a83d7625c5cffafe7ef5c678ad082";
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    try {
+      const response = await fetch(URL);
+
+      if (!response.ok) {
+        throw new Error("Defined city was not found");
+      }
+
+      const data = await response.json();
+
+      setWeather(data);
+    } catch (error) {
+      console.log("Error: ${error.message}");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter your city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <button onClick={fetchWeather}>Click</button>
+      {weather && <WeatherDisplay weather={weather} />}
     </div>
   );
 }
---
+++ src/components/WeatherDisplay.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface Weather {
+  name: string;
+  sys: { country: string };
+  main: { temp: number; humidity: number };
+  weather: [{ description: string }];
+  wind: { speed: number };
+  timezone: number;
+}
+
+function WeatherDisplay({ weather }: { weather: Weather }) {
+  return (
+    <>
+      <h2>
+        {weather.name}, {weather.sys.country}
+      </h2>
+      <p>Temperature: {weather.main.temp}°C</p>
+      <p>Condition: {weather.weather[0].description}</p>
+      <p>Humidity: {weather.main.humidity}%</p>
+      <p>Wind Speed: {weather.wind.speed} m/s</p>
+      <p>Timezone: {weather.timezone}</p>
+    </>
+  );
+}
+
+export default WeatherDisplay;
